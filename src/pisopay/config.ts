5cb3872a67dc29597f68a98246ed8f3b4d8a974f
Arnold <arnoldsulana.seaversity@gmail.com>
2022-11-18 18:27:51 +0800

    change env variables on config ts file

--- src/pisopay/config.ts
+++ src/pisopay/config.ts
@@ -2,6 +2,6 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const USER = process.env.PISOPAY_XGATEWAYAUTH;
-export const PASSWORD = process.env.PISOPAY_XGATEWAYAUTH;
+export const USER = process.env.PISOPAY_USER;
+export const PASSWORD = process.env.PISOPAY_PASSWORD;
 export const XGATEWAY = process.env.PISOPAY_XGATEWAYAUTH;
